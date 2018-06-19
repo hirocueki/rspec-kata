90acd2059d1357113e8d95ca9bfabfd96b1dad6f
hirocueki <hirocueki@gmail.com>
2018-06-20 08:54:27 +0900

    Add user authentication token for API access

--- rspec-kata/db/schema.rb
+++ rspec-kata/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180619065106) do
+ActiveRecord::Schema.define(version: 20180619235109) do
 
   create_table "notes", force: :cascade do |t|
     t.text "message"
