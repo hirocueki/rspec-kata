59d2f1f72743558e9ac0dcd4ec7f255e7712bfc7
hirocueki <hirocueki@gmail.com>
2018-06-19 17:44:53 +0900

    Add Tasks scaffold

--- rspec-kata/config/routes.rb
+++ rspec-kata/config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  resources :projects
+  resources :tasks
+  devise_for :users, controllers: { registrations: 'registrations'}
+
+   authenticated :user do
+    root 'projects#index', as: :authenticated_root
+  end
+
+  resources :projects do
+    resources :notes
+    resources :tasks do
+      member do
+        post :toggle
+      end
+    end
+  end
+
+  namespace :api do
+    resources :projects
+  end
+
   root 'homes#index'
 end
