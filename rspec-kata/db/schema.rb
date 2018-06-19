8d70c4dd43bf067bd07e6d26e888c8f40443d1b6
ks-developer <ksapp@aok-net.com>
2018-06-19 14:07:42 +0900

    Restyle project views with Bootstrap

--- rspec-kata/db/schema.rb
+++ rspec-kata/db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 20180613213220) do
 
   create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "due_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
