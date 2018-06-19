fe92e843cb457a8e73037f6e53e7e08d22588927
ks-developer <ksapp@aok-net.com>
2018-06-19 14:57:15 +0900

    Associate user with projects

--- rspec-kata/app/controllers/projects_controller.rb
+++ rspec-kata/app/controllers/projects_controller.rb
@@ -1,30 +1,22 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = current_user.projects
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.new(project_params)
 
     respond_to do |format|
       if @project.save
--- rspec-kata/app/models/user.rb
+++ rspec-kata/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  has_many :projects
 
   def name
     [first_name, last_name].join(" ")
